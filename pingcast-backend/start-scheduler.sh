473f7e06556369e6edde96693a663c1d261a2a54
azuh salvation <salvationazuh@gmail.com>
2026-08-01 08:43:11 +0100

    fix the scheduler file

--- pingcast-backend/start-scheduler.sh
+++ pingcast-backend/start-scheduler.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+mkdir -p /tmp/empty
+
 php artisan schedule:work &
 SCHEDULER_PID=$!
 
-php -S 0.0.0.0:$PORT -t /dev/null &
+php -S 0.0.0.0:$PORT -t /tmp/empty &
 SERVER_PID=$!
 
 wait $SCHEDULER_PID $SERVER_PID
